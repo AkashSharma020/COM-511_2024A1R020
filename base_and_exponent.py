e6c104e63f143ce1a98f08759dacd7ab86943bf3
AkashSharma020 <akashsharma30614@gmail.com>
2026-08-25 12:54:47 +0530

    Create base_and_exponent.py

---
+++ base_and_exponent.py
@@ -0,0 +1,8 @@
+# Write a  program to take a base number  x and an exponent y as input and calculates x raised to power y 
+
+
+x = int(input("Enter Your Base: "))
+y = int(input("Enter Your Exponent: "))
+
+
+print("Base to the power Exponent =", pow(x,y))
