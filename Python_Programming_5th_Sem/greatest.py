8662d1488a4336b0201af4b03122169f6763e367
AkashSharma020 <akashsharma30614@gmail.com>
2026-09-10 11:49:41 +0530

    Create greatest.py

---
+++ Python_Programming_5th_Sem/greatest.py
@@ -0,0 +1,12 @@
+# Write a program to input 4 numbers from the user and find the greatest number among them. 
+
+
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+num3 = int(input("Enter 3rd number: "))
+num4 = int(input("Enter 4th number: "))
+
+greatest = max(num1, num2, num3, num4)
+
+print("The Greatest number is:", greatest)
