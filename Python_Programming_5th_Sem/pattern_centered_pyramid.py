2bedda9e53c6acf1cce24519235bdd21d3bb4448
AkashSharma020 <akashsharma30614@gmail.com>
2026-09-16 12:02:48 +0530

    Create pattern_centered_pyramid.py

---
+++ Python_Programming_5th_Sem/pattern_centered_pyramid.py
@@ -0,0 +1,8 @@
+# 14. Write a Python program to print a centred pyramid usibg stars.
+
+n = int(input("Enter number of rows: "))
+
+for i in range(1, n + 1):
+    spaces = n - i
+    stars = 2 * i - 1
+    print(" " * spaces + "*" * stars)
